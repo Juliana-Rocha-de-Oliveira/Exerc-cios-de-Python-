8adac1b6a9aa09775d3c168d9ce7725193cbc809
Juliana Rocha de Oliveira <92902343+Juliana-Rocha-de-Oliveira@users.noreply.github.com>
2022-03-08 19:05:51 -0300

    Add files via upload

---
+++ Desafio 01 correto.py
@@ -0,0 +1,4 @@
+nome = input('Qual é o seu nome?')
+idade = input('Quanto anos você tem?')
+peso = input ('Quanto você pesa ?')
+print(nome, idade, peso)
---
+++ Desafio 02 correto.py
@@ -0,0 +1,7 @@
+x = input('Dia')
+y = input('Mes')
+z = input('Ano')
+print('Você nasceu no dia',x,'de',y,'de',z,'correto?')
+
+
+
